1d1cec9c828ed4ae1b2731f70b8566aad8b166e7
wanqing-123 <31007354@qq.com>
2016-04-07 11:54:30 +0800

    add app.java

---
+++ JDesign/src/com/ayalamih/design/app/App.java
@@ -0,0 +1,15 @@
+package com.ayalamih.design.app;
+/**
+ * App.java
+ * JDesign
+ * 
+ * Created by ayalamih on 2016年4月7日 上午11:50:55 TODO
+ * Copyright (c) 2016 __AYALAMIH__. All rights reserved.
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("j design");
+	}
+
+}
