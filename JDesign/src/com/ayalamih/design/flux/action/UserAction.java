4d4826f80eabc5cd3d661fc602a6e4d601ebf034
wanqing-123 <31007354@qq.com>
2016-04-07 13:41:50 +0800

    a

---
+++ JDesign/src/com/ayalamih/design/flux/action/UserAction.java
@@ -0,0 +1,11 @@
+package com.ayalamih.design.flux.action;
+/**
+ * UserAction.java
+ * JDesign
+ * 
+ * Created by ayalamih on 2016年4月7日 下午1:40:31 TODO
+ * Copyright (c) 2016 __AYALAMIH__. All rights reserved.
+ */
+public class UserAction extends Action {
+
+}
