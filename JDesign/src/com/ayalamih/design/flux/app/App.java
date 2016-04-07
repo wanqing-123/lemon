19d9f857bc844c4c4092a0de22015836162743ec
wanqing-123 <31007354@qq.com>
2016-04-07 13:36:58 +0800

    flux design

---
+++ JDesign/src/com/ayalamih/design/flux/app/App.java
@@ -0,0 +1,11 @@
+package com.ayalamih.design.flux.app;
+/**
+ * App.java
+ * JDesign
+ * 
+ * Created by ayalamih on 2016年4月7日 下午1:35:38 TODO
+ * Copyright (c) 2016 __AYALAMIH__. All rights reserved.
+ */
+public class App {
+
+}
