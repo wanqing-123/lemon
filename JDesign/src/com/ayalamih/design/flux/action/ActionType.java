536275d816876a2d4996777b4c1792b10e10c47a
wanqing-123 <31007354@qq.com>
2016-04-08 15:36:12 +0800

    添加操作类型

---
+++ JDesign/src/com/ayalamih/design/flux/action/ActionType.java
@@ -0,0 +1,11 @@
+package com.ayalamih.design.flux.action;
+/**
+ * ActionType.java
+ * JDesign
+ * 
+ * Created by ayalamih on 2016年4月8日 下午3:34:22 TODO
+ * Copyright (c) 2016 __AYALAMIH__. All rights reserved.
+ */
+public enum ActionType {
+
+}
